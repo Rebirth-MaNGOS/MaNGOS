2073ef440e76e79bb6dfe1216b93f20c17d6836c
Revontulet <revontulet@nonexistent.fake>
2015-04-25 16:54:08 +0200

    Mark of nature is no longer a buff

--- src/game/SpellMgr.cpp
+++ src/game/SpellMgr.cpp
@@ -708,6 +708,8 @@ bool IsPositiveEffect(SpellEntry const *spellproto, SpellEffectIndex effIndex)
 		return false;
 	case 23444:	// Ultrasafe Transporter - Gadgetzan, malfunction(poly) is not a positive effect.
 		return false;
+	case 25040:	// Mark of Nature is not a positive effect
+		return false;
     }
             
     if ((spellproto->Attributes & SPELL_ATTR_HIDDEN) != 0)
