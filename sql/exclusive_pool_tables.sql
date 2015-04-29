43826030721847d8abbb5ac4a5569f23cd847f48
andkem <kempe@lysator.liu.se>
2015-04-29 09:42:19 +0200

    Moved the exclusive pool tables to the correct DB.
    The exclusive pool tables were incorrectly added to the zp_charactersd
    database instead of the zp_mangosd database.

--- sql/exclusive_pool_tables.sql
+++ sql/exclusive_pool_tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE `zp_charactersd`.`exclusive_pool_creature` (
+CREATE TABLE `zp_mangosd`.`exclusive_pool_creature` (
       `id` INT NOT NULL AUTO_INCREMENT,
       `poolID` INT NULL,
       `groupID` INT NULL DEFAULT 0,
       `guid` INT NOT NULL,
       PRIMARY KEY (`id`));
 
-CREATE TABLE `zp_charactersd`.`exclusive_pool_spot` (
+CREATE TABLE `zp_mangosd`.`exclusive_pool_spot` (
       `id` INT NOT NULL AUTO_INCREMENT,
       `poolID` INT NULL,
       `mapID` INT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE `zp_charactersd`.`exclusive_pool_spot` (
       `z` FLOAT NULL,
       PRIMARY KEY (`id`));
 
-CREATE TABLE `zp_charactersd`.`exclusive_pool` (
+CREATE TABLE `zp_mangosd`.`exclusive_pool` (
       `id` INT NOT NULL AUTO_INCREMENT,
       `poolID` INT NULL,
       `respawnTime` INT NULL,
