35016ffcd4c55c09a304b6bded81d4c2e11ca5d2
Revontulet <revontulet@nonexistent.fake>
2015-12-18 17:20:22 +0100

    Naxx, Faerlina updated script

--- src/bindings/Scriptdevzero/scripts/eastern_kingdoms/naxxramas/boss_faerlina.cpp
+++ src/bindings/Scriptdevzero/scripts/eastern_kingdoms/naxxramas/boss_faerlina.cpp
@@ -46,28 +46,12 @@ enum
     NPC_NAXXRAMAS_WORSHIPPER = 16506
 };
 
-static Loc WorshiperSpawn[]=
-{
-    {3350.61f, -3619.74f, 261.18f, 4.65f},
-    {3341.36f, -3619.35f, 261.18f, 4.68f},
-    {3356.69f, -3621.17f, 261.18f, 4.38f},
-    {3364.08f, -3622.85f, 261.18f, 4.35f}
-};
-
-static Loc FollowerSpawn[]=
-{
-    {3359.75f, -3621.77f, 261.18f, 4.54f},
-    {3346.29f, -3619.32f, 261.18f, 4.61f}
-};
-
 struct MANGOS_DLL_DECL boss_faerlinaAI : public ScriptedAI
 {
     boss_faerlinaAI(Creature* pCreature) : ScriptedAI(pCreature)
     {
         m_pInstance = (instance_naxxramas*)pCreature->GetInstanceData();
         m_bHasTaunted = false;
-        SpawnAdds();
-        m_bRespawnAdds = false;
         Reset();
     }
 
@@ -78,20 +62,11 @@ struct MANGOS_DLL_DECL boss_faerlinaAI : public ScriptedAI
     uint32 m_uiEnrageTimer;
     bool   m_bHasTaunted;
     
-   bool m_bRespawnAdds; 
-    
-    GUIDList m_uiFollowerGUID;
-    GUIDList m_uiWorshipperGUID;
-    
     void Reset()
     {
         m_uiPoisonBoltVolleyTimer = 8000;
         m_uiRainOfFireTimer = 16000;
-        m_uiEnrageTimer = 60000;
-        
-        // respawn adds 
-        if(m_bRespawnAdds)
-            RespawnAdds();
+        m_uiEnrageTimer = 60000;       
     }
 
     void Aggro(Unit* /*pWho*/)
@@ -107,44 +82,7 @@ struct MANGOS_DLL_DECL boss_faerlinaAI : public ScriptedAI
         if (m_pInstance)
             m_pInstance->SetData(TYPE_FAERLINA, IN_PROGRESS);
     }
-
-    void JustSummoned(Creature* pSummoned)
-    {
-            if(pSummoned->GetEntry() == NPC_NAXXRAMAS_FOLLOWER)
-                m_uiFollowerGUID.push_back(pSummoned->GetObjectGuid());
-            if(pSummoned->GetEntry() == NPC_NAXXRAMAS_WORSHIPPER)
-                m_uiWorshipperGUID.push_back(pSummoned->GetObjectGuid());     
-    }
-    
-    void SummonedCreatureJustDied(Creature* pSummoned)
-    {
-        if(pSummoned->GetEntry() == NPC_NAXXRAMAS_WORSHIPPER)
-            m_creature->CastSpell(m_creature, SPELL_WIDOWS_EMBRACE, true);
-        m_bRespawnAdds = true;
-    }
-    
-    void SpawnAdds()
-    {
-        for(uint8 i = 0; i < 4; ++i)
-            m_creature->SummonCreature(NPC_NAXXRAMAS_WORSHIPPER, WorshiperSpawn[i].x, WorshiperSpawn[i].y, WorshiperSpawn[i].z, WorshiperSpawn[i].o, TEMPSUMMON_DEAD_DESPAWN, 0);
-        for(uint8 i = 0; i < 2; ++i)
-            m_creature->SummonCreature(NPC_NAXXRAMAS_FOLLOWER, FollowerSpawn[i].x, FollowerSpawn[i].y, FollowerSpawn[i].z, FollowerSpawn[i].o, TEMPSUMMON_DEAD_DESPAWN, 0);
-    }
-    
-    void RespawnAdds()
-    {       
-        for (GUIDList::iterator itr = m_uiFollowerGUID.begin(); itr != m_uiFollowerGUID.end(); itr++)
-            if (Creature* pFollower = m_creature->GetMap()->GetCreature(*itr))
-                if (!pFollower->isAlive())
-                    pFollower->Respawn();
-         for (GUIDList::iterator itr = m_uiWorshipperGUID.begin(); itr != m_uiWorshipperGUID.end(); itr++)
-            if (Creature* pWorshipper = m_creature->GetMap()->GetCreature(*itr))
-                if (!pWorshipper->isAlive())
-                    pWorshipper->Respawn();     
-                
-        m_bRespawnAdds = false;
-    }
-    
+        
     void MoveInLineOfSight(Unit* pWho)
     {
         if (!m_bHasTaunted && pWho->GetTypeId() == TYPEID_PLAYER && m_creature->IsWithinDistInMap(pWho, 80.0f) &&  m_creature->IsWithinLOSInMap(pWho))
@@ -168,11 +106,37 @@ struct MANGOS_DLL_DECL boss_faerlinaAI : public ScriptedAI
         if (m_pInstance)
             m_pInstance->SetData(TYPE_FAERLINA, DONE);
     }
-    
-     void JustReachedHome()
+        
+    void JustReachedHome()
     {
         if (m_pInstance)
             m_pInstance->SetData(TYPE_FAERLINA, FAIL);
+        
+        // Respawn Worshippers 
+        std::list<Creature*> m_lWorshippers;
+        GetCreatureListWithEntryInGrid(m_lWorshippers, m_creature, NPC_NAXXRAMAS_WORSHIPPER, DEFAULT_VISIBILITY_INSTANCE);
+
+        if (!m_lWorshippers.empty())
+        {
+            for(std::list<Creature*>::iterator itr = m_lWorshippers.begin(); itr != m_lWorshippers.end(); ++itr)
+            {
+                if ((*itr) && !(*itr)->isAlive())
+                    (*itr)->Respawn();
+            }
+        }
+        
+        // Respawn followers
+        std::list<Creature*> m_lFollowers;
+        GetCreatureListWithEntryInGrid(m_lFollowers, m_creature, NPC_NAXXRAMAS_FOLLOWER, DEFAULT_VISIBILITY_INSTANCE);
+
+        if (!m_lFollowers.empty())
+        {
+            for(std::list<Creature*>::iterator itr = m_lFollowers.begin(); itr != m_lFollowers.end(); ++itr)
+            {
+                if ((*itr) && !(*itr)->isAlive())
+                    (*itr)->Respawn();
+            }
+        }        
     }
     
     // Widow's Embrace prevents frenzy and poison bolt, if it removes frenzy, next frenzy is sceduled in 60s
@@ -239,11 +203,137 @@ CreatureAI* GetAI_boss_faerlina(Creature* pCreature)
     return new boss_faerlinaAI(pCreature);
 }
 
+/*######
+## mob_naxxramas_worshipper
+######*/
+
+struct MANGOS_DLL_DECL mob_naxxramas_worshipperAI : public ScriptedAI
+{
+    mob_naxxramas_worshipperAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        m_pInstance = (instance_naxxramas*)pCreature->GetInstanceData();
+        Reset();
+    }
+
+    instance_naxxramas* m_pInstance;
+
+    void Reset()
+    {
+    }
+    
+    void JustDied(Unit *)
+    {
+        if (m_pInstance)
+            {            
+                if(Creature* pFaerlina = m_pInstance->GetSingleCreatureFromStorage(NPC_FAERLINA))
+                    if (pFaerlina->isAlive() && m_pInstance->GetData(TYPE_FAERLINA) == IN_PROGRESS)
+                        pFaerlina->CastSpell(pFaerlina, SPELL_WIDOWS_EMBRACE, true);            // worshipper should cast on boss?
+            }
+    }
+        
+    void UpdateAI(const uint32 uiDiff)
+    {
+        // Return if we have no target
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+            return;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_mob_naxxramas_worshipper(Creature* pCreature)
+{
+    return new mob_naxxramas_worshipperAI(pCreature);
+}
+
+/*######
+## mob_naxxramas_follower
+######*/
+
+enum eNaxxramasFollower
+{
+    SPELL_CHARGE = 22911,       // not sure which spell
+    SPELL_SILENCE  = 29943       // not sure which spell
+};
+
+struct MANGOS_DLL_DECL mob_naxxramas_followerAI : public ScriptedAI
+{
+    mob_naxxramas_followerAI(Creature* pCreature) : ScriptedAI(pCreature)
+    {
+        m_pInstance = (instance_naxxramas*)pCreature->GetInstanceData();
+        Reset();
+    }
+
+    instance_naxxramas* m_pInstance;
+
+    uint32 m_uiChargeTimer;
+    uint32 m_uiSilenceTimer;
+    uint32 m_uiSuspendTimer;
+
+    void Reset()
+    {
+        m_uiChargeTimer = urand(8000,15000);
+        m_uiSilenceTimer = urand(6000,20000);
+        m_uiSuspendTimer = 0;
+    }
+    
+    void UpdateAI(const uint32 uiDiff)
+    {
+        // Return if we have no target
+        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
+            return;
+        
+        // for charge animation
+        if (m_uiSuspendTimer > uiDiff)
+        {
+            m_uiSuspendTimer -= uiDiff;
+            return;
+        }
+        
+        // Charge
+        if (m_uiChargeTimer <= uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+                if(DoCastSpellIfCan(pTarget, SPELL_CHARGE)  == CAST_OK)
+                    m_uiSuspendTimer = 2000;
+            m_uiChargeTimer = urand(17000,22000);
+        }
+        else
+            m_uiChargeTimer -= uiDiff;
+
+        // Silence
+        if (m_uiSilenceTimer <= uiDiff)
+        {
+            DoCastSpellIfCan(m_creature->getVictim(), SPELL_SILENCE);
+            m_uiSilenceTimer = urand(15000,25000);
+        }
+        else
+            m_uiSilenceTimer -= uiDiff;
+
+        DoMeleeAttackIfReady();
+    }
+};
+
+CreatureAI* GetAI_mob_naxxramas_follower(Creature* pCreature)
+{
+    return new mob_naxxramas_followerAI(pCreature);
+}
+
 void AddSC_boss_faerlina()
 {
     Script* pNewscript;
     pNewscript = new Script;
     pNewscript->Name = "boss_faerlina";
     pNewscript->GetAI = &GetAI_boss_faerlina;
+    pNewscript->RegisterSelf();
+    
+    pNewscript = new Script;
+    pNewscript->Name = "mob_naxxramas_follower";
+    pNewscript->GetAI = &GetAI_mob_naxxramas_follower;
+    pNewscript->RegisterSelf();
+    
+    pNewscript = new Script;
+    pNewscript->Name = "mob_naxxramas_worshipper";
+    pNewscript->GetAI = &GetAI_mob_naxxramas_worshipper;
     pNewscript->RegisterSelf();
 }
