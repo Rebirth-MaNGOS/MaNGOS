efa560ce880276c301437b1e6ca620fba962f2b4
Flof <Flof@nonexistent.fake>
2016-04-30 14:03:40 +0200

    Toggling pet to passive should return it to the player.

--- src/game/PetHandler.cpp
+++ src/game/PetHandler.cpp
@@ -191,6 +191,11 @@ void WorldSession::HandlePetAction(WorldPacket& recv_data)
         switch (spellid)
         {
         case REACT_PASSIVE:                         // passive
+            if(pet->isInCombat())
+            {
+                pet->AttackStop();
+                pet->GetMotionMaster()->MoveFollow(_player, PET_FOLLOW_DIST,PET_FOLLOW_ANGLE);
+            }
         case REACT_DEFENSIVE:                       // recovery
         case REACT_AGGRESSIVE:                      // activete
             charmInfo->SetReactState(ReactStates(spellid));
