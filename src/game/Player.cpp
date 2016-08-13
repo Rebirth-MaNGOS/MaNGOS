ef3d8791c20a9a91cf74c41ecfdf5406529d627f
Revontulet <revontulet@nonexistent.fake>
2016-08-13 22:01:46 +0200

    Feline swiftness shouldn't be applied to other forms than cat form

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -5888,16 +5888,21 @@ void Player::CheckAreaExploreAndOutdoor()
             }
         }
         // If the player is a druid, update pvp speed bonus, add passive speed bonus aswell
-        if (getClass() == CLASS_DRUID && (HasAura(768) || HasAura(5487) || HasAura(9634) || HasAura(783)))
-        {            
-            UpdateEquipSpellsAtFormChange();
-            
-            // apply the talent speed boost
-            if (HasSpell(17002))
-                CastSpell(this, 17002, true);
-            if (HasSpell(24866))
-                CastSpell(this, 24866, true);            
-        }
+        if (getClass() == CLASS_DRUID)
+        {               
+            // Handle passive speed bonus from talents separately
+            if (HasAura(768))
+            {
+                // apply the talent speed boost
+                if (HasSpell(17002))
+                    CastSpell(this, 17002, true);
+                if (HasSpell(24866))
+                    CastSpell(this, 24866, true);     
+            }
+            // PvP speed boost 
+            if (HasAura(768) || HasAura(5487) || HasAura(9634) || HasAura(783))
+                UpdateEquipSpellsAtFormChange();                          
+    
     }
     else if (sWorld.getConfig(CONFIG_BOOL_VMAP_INDOOR_CHECK) && !isGameMaster())
         RemoveAurasWithAttribute(SPELL_ATTR_OUTDOORS_ONLY);
